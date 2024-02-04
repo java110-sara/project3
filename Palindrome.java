638403b17e8a0c174e243f10640e049ecbd6724f
sara russert <sarabeth.russert@gmail.com>
2024-02-04 09:37:20 -0800

    adds additional logging

--- Palindrome.java
+++ Palindrome.java
@@ -10,25 +10,34 @@ public class Palindrome {
       Scanner keyboard = new Scanner(System.in);
       String userInput;
       Boolean isPalindrome = true;
+      int counter = 0;
       
       // get input from the user
-      System.out.println("This is a palindrome validator...");
-      System.out.print("Please enter a string to see if it is a palindrome:  ");
+      // System.out.println("This is a palindrome validator...");
+      System.out.print("Enter a string:  ");
       userInput = keyboard.nextLine();
       
       // asses if input is a palindrome
-      for (int i = 0; i < userInput.length(); i++) {
-         if (i >= userInput.length()/2) {
-            isPalindrome = userInput.regionMatches(false, i, userInput, userInput.length()-1-i, 1);
-         }
-         if (isPalindrome == false) {
-            System.out.println("Your string, " + userInput + ", is not a palindrome.");
-            System.exit(0);
+      for (int i = 0, j = userInput.length()-1; i < userInput.length(); i++, j--) {
+         if (i <= userInput.length()/2) {
+            boolean stringMatch = userInput.regionMatches(false, i, userInput, j, 1);
+            if (stringMatch == false) {
+               counter++;
+               System.out.println("The chars at " + i + " and " + j + " do not match.");
+               if (isPalindrome == true)
+                  isPalindrome = false;
+            }
          }
       }
-      
-      // if you made through the loop it is a palindrome
-      System.out.println("Congratulations! Your string, " + userInput + ", is a palindrome.");
+
+      // if invalid palindrome
+      if (isPalindrome == false) {
+            System.out.println(userInput + " is not a valid palindrome.");
+            System.out.println("The number of invalid character matches: " + counter);
+            System.exit(0);
+      }
+      // if valid palindrome
+      System.out.println(userInput + " is a valid palindrome.");
       System.exit(0);
       
       
